5dbf41b7da6d47ec915325710c0e0673c1b649f4
ckulhanjian <carak143@gmail.com>
2026-03-10 21:26:03 -0400

    updated sign up and allergy selection

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,35 @@
-import { Stack } from "expo-router";
+import { Stack, router } from "expo-router";
+import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { AuthProvider } from "../AuthContext";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { AuthProvider, useAuth } from "../AuthContext";
 import "../global.css";
 
 export default function RootLayout() {
-  return (
+  const { user, loading } = useAuth();
+
+  useEffect(() => {
+    if (loading) return; // wait until Firebase responds
+
+    if (user) {
+      router.replace('/home');  // logged in → go to home
+    } else {
+      router.replace('/');        // not logged in → go to login
+    }
+  }, [user, loading]);
+
+    return (
     <AuthProvider>
+      <SafeAreaProvider>
       <GestureHandlerRootView style={{ flex: 1 }}>
-        {/* By leaving screenOptions here, it applies to all screens.
-            We don't need to list index, home, or modal manually! 
-        */}
-        <Stack screenOptions={{ headerShown: false }} />
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" options={{ animation: 'none' }} />
+          <Stack.Screen name="signup" options={{ animation: 'slide_from_right' }} />
+          <Stack.Screen name="allergy" options={{ animation: 'none' }} />
+          <Stack.Screen name="home" options={{ animation: 'none' }} />
+        </Stack>
       </GestureHandlerRootView>
+      </SafeAreaProvider>
     </AuthProvider>
   );
 }
